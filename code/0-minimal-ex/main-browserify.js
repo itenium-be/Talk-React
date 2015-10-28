7dd7ae22a67d047899eeb430b7b3385fe15cba77
Laoujin <woutervs@hotmail.com>
2015-10-29 00:53:27 +0100

    code0: removed undefined/null tests

--- code/0-minimal-ex/main-browserify.js
+++ code/0-minimal-ex/main-browserify.js
@@ -7,10 +7,6 @@ ReactDOM.render(
   <div>
     <h1>Hello, world!</h1>
     {blah}
-    <h1>Undefined</h1>
-    {undefined}
-    <h1>null</h1>
-    {null}
   </div>,
   document.getElementById('example')
 );
--- code/0-minimal-ex/main.js
+++ code/0-minimal-ex/main.js
@@ -4,10 +4,6 @@ ReactDOM.render(
   <div>
     <h1>Hello, world!</h1>
     {blah}
-    <h1>Undefined</h1>
-    {undefined}
-    <h1>null</h1>
-    {null}
   </div>,
   document.getElementById('example')
 );
